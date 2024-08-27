0441864a903c3821046a3a50b30f420bf372e2d7
Andresumng2024 <est.andres.sanchezma@unimilitar.edu.co>
2024-08-27 15:20:35 -0500

    Create pg1.java
    //crear una clase llamada triangulo, con atributos, base y altura , aplicar encapsulamiento , y los metodos apropiados para inicializar o crear objetos con sus respectivos atributos

---
+++ P1/pg1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.triangulo;
+
+/**
+ *
+ * @author Multipropósito2
+ */
+public class Triangulo {
+
+
+private int Base;
+private int Altura;
+
+  
+
+    public int getBase() {
+        return Base;
+    }
+
+    public void setBase(int base) {
+        this.Base = base;
+    }
+
+    public int getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(int altura) {
+        this.Altura = altura;
+    }
+
+    public static void main(String[] args) {
+    
+     
+        
+    }
+}
